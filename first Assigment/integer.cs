b138f72b8ac4c9b7a88c7b6952f01c893a923636
nitemi@ <blessedtgirl@gmail.com>
2023-11-17 17:54:30 +0100

    changes

--- first Assigment/integer.cs
+++ first Assigment/integer.cs
@@ -9,16 +9,30 @@ namespace first_Assigment
     public class Interactive
 
     {
-        switch
+
         public Interactive()
 
 
         {
-            Console.WriteLine("Please select a coffee 1-3")
- 
-                Case 1 : "coffee 1 is #3000"
-                break;
+            Console.WriteLine("Please select a coffee 1-3");
+            int coffee = int.Parse(Console.ReadLine());
 
-            
+            switch (coffee)
+            {
+                case 1:
+                    Console.WriteLine("coffee 1 is #3000");
+                    break;
+                    case 2:
+                    Console.WriteLine("coffee 2 is #5000");
+                    break;
+                    case 3:
+                    Console.WriteLine("coffee 3 is #7500");
+                    break;
+
+                    default: 
+                    Console.WriteLine("invalid order");
+                    break;
+            }
+        }
     }
 }
